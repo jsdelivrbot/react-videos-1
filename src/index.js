1529408182bedb56da5ca2cee426f592d6829f9b
Vinícius Michelutti <vinicius@meuspedidos.com.br>
2017-01-24 14:57:06 -0200

    Video detail and player

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import YTSearch from 'youtube-api-search'
 
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list'
+import VideoDetail from './components/video_detail'
 
 const API_KEY = "AIzaSyDwMZ8d7KNq8IlghCcnXzQClLXIzlxULuQ"
 
@@ -13,7 +14,7 @@ class App extends Component {
         super(props);
         this.state = { videos: [] };
 
-        YTSearch({ key: API_KEY, term: 'hiphop' }, (videos) => {
+        YTSearch({ key: API_KEY, term: 'hip hop' }, (videos) => {
             console.log(videos);
             this.setState({ videos }) // shorthanded es6
         });
@@ -23,6 +24,7 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
+                <VideoDetail video={this.state.videos[0]} />
                 <VideoList videos={this.state.videos} />
             </div>
         );
